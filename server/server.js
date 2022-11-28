ff4c79c9c484d5e01aca60872ebcfd5a40fbc567
Deoksoo(Eden) <deoksoo.lee@smartfoodnet.com>
2022-11-28 20:50:39 +0900

    image upload server prototype complete

--- server/server.js
+++ server/server.js
@@ -8,10 +8,22 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) =>
     cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
 });
-const upload = multer({ storage });
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (["image/png", "image/jpeg"].includes(file.mimetype)) cb(null, true);
+    else cb(new Error("Invalid File Type"), false);
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
 
 const app = express();
 const PORT = 5000;
+
+app.use("uploads", express.static("uploads"));
 
 app.post("/upload", upload.single("image"), (req, res) => {
   console.log(req.file);
