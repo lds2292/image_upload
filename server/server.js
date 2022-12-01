1210de4f3d0d12da1db72763f95637b0522212f0
Eden.lee home <lds2292@gmail.com>
2022-12-01 23:39:30 +0900

    FIX: 리팩터링 라우트 추가 미들웨어 추가

--- server/server.js
+++ server/server.js
@@ -1,52 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const multer = require("multer");
-const { v4: uuid } = require("uuid");
-const mime = require("mime-types");
 const mongoose = require("mongoose");
-const Image = require("./models/Image");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "./uploads"),
-  filename: (req, file, cb) =>
-    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
-});
-
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    if (["image/png", "image/jpeg"].includes(file.mimetype)) cb(null, true);
-    else cb(new Error("Invalid File Type"), false);
-  },
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
-});
+const { imageRouter } = require("./routes/imageRouter");
 
 const app = express();
-const PORT = 5000;
+const { MONGO_URL, PORT } = process.env;
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("MongoDB Connected");
+
     app.use("/uploads", express.static("uploads"));
 
-    app.post("/upload", upload.single("image"), async (req, res) => {
-      const image = await new Image({
-        key: req.file.filename,
-        originalFileName: req.file.originalname,
-      }).save();
-      res.json(image);
-    });
-
-    app.get("/images", async (req, res) => {
-      const images = await Image.find();
-      res.json(images);
-    });
+    app.use("/images", imageRouter);
 
     app.listen(PORT, () => {
       console.log("Express server listening on PORT = " + PORT);
