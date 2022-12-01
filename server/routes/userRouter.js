c2ac9cc38065cb7dfdb87bd4a1e3d1242282c995
Eden.lee home <lds2292@gmail.com>
2022-12-02 00:15:26 +0900

    FEAT: User Register, Login Implement

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,32 @@
 const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
+const { hash, compare } = require("bcryptjs");
 
 userRouter.post("/register", async (req, res) => {
-  const user = await new User(req.body).save();
-  res.json({ message: "user registered" });
+  try {
+    if (req.body.password.length < 6) throw new Error("비밀번호 6자 이상");
+    const hashedPassword = await hash(req.body.password, 10);
+    const user = await new User({
+      name: req.body.name,
+      username: req.body.username,
+      hashedPassword,
+    }).save();
+    res.json({ message: "user registered" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    const isValid = await compare(req.body.password, user.hashedPassword);
+    if (!isValid) throw new Error("입력하신 정보가 올바르지 않습니다");
+    res.json({ message: "user validated" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 module.exports = { userRouter };
