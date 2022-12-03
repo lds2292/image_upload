981e930f5102893658fa49eb5a3ac2f7de100bf6
Eden.lee home <lds2292@gmail.com>
2022-12-03 22:33:20 +0900

    Feat: Toolbar added

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import RegisterPage from "./pages/RegisterPage";
 import { Switch, Route } from "react-router-dom";
+import ToolBar from "./component/ToolBar";
 
 // react-router-dom의 버전 6에서는 Swtich -> Routes로 변경되었다
 // Route 안의 component -> element로 변경되었다
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div style={{ maxWidth: 600, margin: "auto" }}>
       <ToastContainer />
+      <ToolBar />
       <Switch>
         <Route path="/auth/register" exact component={RegisterPage} />
         <Route path="/auth/login" exact component={LoginPage} />
