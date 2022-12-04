780073a7ae4415592690d0cb153ea24590428cdf
Eden.lee home <lds2292@gmail.com>
2022-12-04 22:10:15 +0900

    Feat: Client multiple image upload

--- client/src/component/UploadForm.js
+++ client/src/component/UploadForm.js
@@ -8,15 +8,16 @@ import { ImageContext } from "../context/ImageContext";
 const UploadForm = () => {
   const { images, setImages, myImages, setMyImages } = useContext(ImageContext);
   const DEFAULT_MESSAGE = "이미지 파일을 업로드 해주세요";
-  const [file, setFile] = useState(null);
+  const [files, setFiles] = useState(null);
   const [imgSrc, setImgSrc] = useState(null);
   const [fileName, setFileName] = useState(DEFAULT_MESSAGE);
   const [percent, setPercent] = useState(0);
   const [isPublic, setIsPublic] = useState(true);
 
   const imageSelectHandler = (event) => {
-    const imageFile = event.target.files[0];
-    setFile(imageFile);
+    const imageFiles = event.target.files;
+    setFiles(imageFiles);
+    const imageFile = imageFiles[0];
     setFileName(imageFile.name);
     const fileReader = new FileReader();
     fileReader.readAsDataURL(imageFile);
@@ -27,7 +28,11 @@ const UploadForm = () => {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append("image", file);
+
+    for (let file of files) {
+      formData.append("image", file);
+    }
+
     formData.append("public", isPublic);
     try {
       const res = await axios.post("/images", formData, {
@@ -65,6 +70,7 @@ const UploadForm = () => {
         <input
           id="image"
           type="file"
+          multiple
           accept="image/*"
           onChange={imageSelectHandler}
         />
