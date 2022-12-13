5c319d53d600ef794f73c924594c5ba032f679f9
Eden.lee home <lds2292@gmail.com>
2022-12-13 23:03:32 +0900

    Feat: Aws S3 Presigned Url Post added

--- server/aws.js
+++ server/aws.js
@@ -6,4 +6,26 @@ const s3 = new aws.S3({
   accessKeyId: AWS_ACCESS_KEY,
 });
 
-module.exports = { s3 };
+const getSignedUrl = ({ key }) => {
+  return new Promise((resolve, reject) => {
+    s3.createPresignedPost(
+      {
+        Bucket: "browngoo-image-upload-tutorial",
+        Fields: {
+          key,
+        },
+        Expires: 300,
+        Conditions: [
+          ["content-length-range", 0, 50 * 1000 * 1000],
+          ["starts-with", "$Content-Type", "image/"],
+        ],
+      },
+      (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      }
+    );
+  });
+};
+
+module.exports = { s3, getSignedUrl };
